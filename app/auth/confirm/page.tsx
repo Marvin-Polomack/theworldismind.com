7d4160ed65062468cfac69e68df0a24d426eadf8
mpolomack <marvin.polomack@gmail.com>
2025-01-18 13:43:42 +0100

    fix ConfirmPage

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 import AuthConfirm from '@/components/AuthForms/AuthConfirm'
 
-export default function ConfirmPage({ searchParams }: { searchParams: { token_hash: string, type: string } }) {
+export default function ConfirmPage() {
 
   return (
     <div className="m-auto flex flex-col items-center justify-center h-screen">
--- components/AuthForms/AuthConfirm.tsx
+++ components/AuthForms/AuthConfirm.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import TwimLogoBlack from '@/components/icons/TwimLogoBlack';
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 import MagicCard from "@/components/ui/MagicCard";
 import { InputOTPForm } from "@/components/ui/OtpForm/InputOtpForm";
 
 export default function AuthConfirm() {
-  const router = useRouter()
   const searchParams = useSearchParams()
-
-  const handleConfirm = () => {
-    router.push(`/api/auth/confirm?token=${searchParams.get("token_hash")}&type=${searchParams.get("type")}`)
-  }
 
   return (
     <div className="flex flex-col justify-between max-w-lg p-3 m-auto">
